609765551ea8d1649207bb7148d984f134ab243d
ali ghafoori <alighafoori1357@gmail.com>
2023-07-20 15:35:10 +0330

    replace collapsibleMenu arrow icons

--- src/components/sidebar/collapsibleMenu.svelte
+++ src/components/sidebar/collapsibleMenu.svelte
@@ -14,16 +14,24 @@
 		<slot name="icon" />
 		<span class="pl-2 rtl:pr-2 grow"> {text}</span>
 		{#if show}
-			<svg class="fill-current text-purple-700 h-6 w-6 transform transition-transform duration-500 rotate-180" viewBox="0 0 20 20">
-				<path
-					d="M13.962,8.885l-3.736,3.739c-0.086,0.086-0.201,0.13-0.314,0.13S9.686,12.71,9.6,12.624l-3.562-3.56C5.863,8.892,5.863,8.611,6.036,8.438c0.175-0.173,0.454-0.173,0.626,0l3.25,3.247l3.426-3.424c0.173-0.172,0.451-0.172,0.624,0C14.137,8.434,14.137,8.712,13.962,8.885 M18.406,10c0,4.644-3.763,8.406-8.406,8.406S1.594,14.644,1.594,10S5.356,1.594,10,1.594S18.406,5.356,18.406,10 M17.521,10c0-4.148-3.373-7.521-7.521-7.521c-4.148,0-7.521,3.374-7.521,7.521c0,4.147,3.374,7.521,7.521,7.521C14.148,17.521,17.521,14.147,17.521,10"
-				/>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="w-4 h-4 stroke-current stroke-2 text-gray-800 transform duration-500 ease-in-out rotate-90"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
+			>
+				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
 			</svg>
 		{:else}
-			<svg class="fill-current text-purple-700 h-6 w-6 transform transition-transform duration-500" viewBox="0 0 20 20">
-				<path
-					d="M13.962,8.885l-3.736,3.739c-0.086,0.086-0.201,0.13-0.314,0.13S9.686,12.71,9.6,12.624l-3.562-3.56C5.863,8.892,5.863,8.611,6.036,8.438c0.175-0.173,0.454-0.173,0.626,0l3.25,3.247l3.426-3.424c0.173-0.172,0.451-0.172,0.624,0C14.137,8.434,14.137,8.712,13.962,8.885 M18.406,10c0,4.644-3.763,8.406-8.406,8.406S1.594,14.644,1.594,10S5.356,1.594,10,1.594S18.406,5.356,18.406,10 M17.521,10c0-4.148-3.373-7.521-7.521-7.521c-4.148,0-7.521,3.374-7.521,7.521c0,4.147,3.374,7.521,7.521,7.521C14.148,17.521,17.521,14.147,17.521,10"
-				/>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="w-4 h-4 stroke-current stroke-2 text-gray-800 transform duration-500 ease-in-out"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
+			>
+				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
 			</svg>
 		{/if}
 	</a>
--- src/stores/dashboardStore.ts
+++ src/stores/dashboardStore.ts
@@ -1,10 +1,11 @@
 import { writable } from 'svelte/store';
+import { browser } from '$app/environment';
 
 export const showSidebar = writable<boolean>(true);
 export const dark = writable<boolean>(false);
 
 dark.subscribe((x) => {
-	if (document) {
+	if (browser) {
 		const html = document.getElementsByTagName('html')[0];
 		if (!x && html.classList.contains('dark')) html.classList.remove('dark');
 		else if (x && !html.classList.contains('dark')) html.classList.add('dark');
